1744fd235e7ce25b061cd10ddebb5e21f7e19bad
muriel <murielthoms@gmail.com>
2016-10-24 15:09:11 +0200

    Ajout moment.js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 (function () {
-	console.log("Hello World!!");
+	
 	$("#récupérer").click(function(){
-		console.log("hello World"); 
-		var addressIp = ("192.168.1.21:3000/places"); 
-		// .ajax(addressIp).done(ajaxDone).fail(ajaxFailed) 
-	}); 
+		console.log("succ");
+		var addressIp = ("http://192.168.1.21:3000/places"); 
+		$.ajax(addressIp){
+			
+		}
+	});
+	function ajaxDone(data){
+		console.log(done);
+		var places = reponce.places
+		for (var i=0, i<places.length, i++)
+			console.log(places[i]);
+	}
 })(); 
