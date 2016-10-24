452a6c86f575f615228312b2c771d8b3bae34708
muriel <murielthoms@gmail.com>
2016-10-24 14:41:10 +0200

    initialisation du la foncton ajout  button  dans html

---
+++ app.js
@@ -0,0 +1,8 @@
+(function () {
+	console.log("Hello World!!");
+	$("#récupérer").click(function(){
+		console.log("hello World"); 
+		var addressIp = ("192.168.1.21:3000/places"); 
+		// .ajax(addressIp).done(ajaxDone).fail(ajaxFailed) 
+	}); 
+})(); 
