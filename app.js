75351bfa524807c1f7964efd9160358c00c048b0
muriel <murielthoms@gmail.com>
2016-10-24 15:51:05 +0200

    ajout Marine modif recup mdp check + début météo

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 (function () {
 	
-	$("#récupérer").click(function(){
+	$("#recuperer").click(function(){
 		console.log("succ");
 		var addressIp = ("http://192.168.1.21:3000/places"); 
-		$.ajax(addressIp){
-			
-		}
+		$.ajax(addressIp)
+		.done(ajaxDone)
+		.fail(ajaxFail);
+
 	});
-	function ajaxDone(data){
-		console.log(done);
-		var places = reponce.places
-		for (var i=0, i<places.length, i++)
-			console.log(places[i]);
+	function ajaxDone(response){
+		console.log(response);
+		for (var i = 0; i < response.places.length; i++) {
+			var place = response.places[i];
+			console.log(place);
+			$('#list').append('<li>' + place.nom + '</li>');
+			if (place.nom === "IoT Valley") {
+				var MotDePasse = place.password;
+				$('#mdp').html('Mot de passe IoT Valley: ' + MotDePasse);
+			}
+		}
 	}
+	function ajaxFail(response) {
+		console.log(response);
+	}
+
+	$('#meteo').click(function(){
+		console.log('meteo ok');
+		var url = "http://openweathermap.org/";
+		$.ajax(url)
+		.done(meteoDone)
+		.fail(meteoFail);
+
+	});
+	function meteoDone(){
+
+		
+	}
+
+
 })(); 
